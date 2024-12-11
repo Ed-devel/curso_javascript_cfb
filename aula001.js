03c0e54b1fc4aa961e325634398a812e8b6971ac
Ed-devel <edson.courses@proton.me>
2024-12-11 11:59:44 -0300

    Vídeo 1 - Novo Curso de Javascript Completo, Profissional e Moderno - Curso de Javascript Moderno - Aula 01. (index.html, aula001.js)

---
+++ aula001.js
@@ -0,0 +1,10 @@
+// barra barra são usadas para comentários em linha no javascript 
+
+/**
+ * comentário de Bloco
+ * 
+ * console.log("Comentário !");
+ * console.log("Alô mundo ! ! !");
+ */
+
+console.log("FIM !");
