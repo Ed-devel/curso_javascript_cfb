269f8f2dc96cc8c4b5d35aaad763290d084e4b16
Ed-devel <edson.courses@proton.me>
2025-02-11 11:34:27 -0300

    Vídeo 12 - Typeof ou operador unário - Aula 11. (aula011.js)

---
+++ aula011.js
@@ -0,0 +1,17 @@
+/** Vídeo 12 -  - Aula 11 */
+
+"use strict"
+
+/** Typeof é um operador que retorna o tipo da variável ou de um elemento
+ *  É também chamado de operador unário
+*/
+
+let v1 = 10;
+let v2 = "10";
+let v3 = v1===v2;
+let v4 = {nome : "Joao"};
+
+console.log("Valor: "+v1+"  |  tipo: "+typeof(v1) );
+console.log("Valor: "+v2+"  |  tipo: "+typeof(v2) );
+console.log("Valor: "+v3+"  |  tipo: "+typeof(v3) );
+console.log("Valor: "+v4+"  |  tipo: "+typeof(v4) );
