f678c7e16b7e5c052443af38a8982ea246836deb
Ed-devel <edson.courses@proton.me>
2025-02-19 21:28:48 -0300

    Vídeo 19 - WHILE e DO WHILE em Javascript, entenda a diferença - Aula 18.(aula018.js)

---
+++ aula018.js
@@ -0,0 +1,49 @@
+// Vídeo 19 - WHILE  e DO WHILE  em Javascript , entenda a  diferença
+// Aula 18
+
+/** Aqui será verificado a diferença entre o Loop While e o Do While.
+ * Ambos são usados em situações  indefinidas, ou seja, quando não se
+ * sabe a quantidade de vezes que serão iteradas o bloco de comandos.
+ * No 'While' primeiro é executada uma expressão entre  parênteses, e
+ * enquanto  essa expressão for  verdadeira  o  bloco  de comandos  é
+ * executado. Na primeira  iteração, se a expressão  for falsa, então
+ * o bloco nem chega a ser executado nenhuma vez.
+ * Já o 'Do While' inicialmente o bloco de comandos é  executado e só
+ * no fim da  estrutura é que a expressão lógica com as condições são
+ * executadas. Enquanto for  verdadeira o laço se  repete. O bloco de
+ * comando  é executado  pelo menos  uma vez  pois  a expressão  só é
+ * verificada no final da extrutura e  mesmo se der falso na primeira
+ * na primeira verificação, o primeiro laço já foi executado.
+ * 
+ * No 'Do While' há a garantia de que o bloco seja executado pelo
+ * menos uma vez. Já no 'While' NÃO há essa garantia. */
+
+"use strict"
+
+const FUNCAO = require('./separador.js');
+
+let n = 10;
+
+while (n < 10) {
+    console.log("CFB cursos");
+    n++;
+}
+console.log("Fim do Loop While");
+
+FUNCAO.separador(); // ----------------------------------------------
+
+do{
+    console.log("CFB cursos");
+    n++;
+}while (n < 10) 
+console.log("Fim do Loop Do While");
+
+
+
+
+/* OUTPUT
+Fim do Loop While
+-------------------------------------------------------
+CFB cursos
+Fim do Loop Do While
+*/
